ac6cc1bf20a8bb9ebc533186f64d291f4865aa14
Your Name <2384928112@qq.com>
2021-10-10 21:22:16 +0800

    fourth

--- src/main/java/Xu/Cal.java
+++ src/main/java/Xu/Cal.java
@@ -7,18 +7,18 @@ public class Cal {
         return a + b;
     }
     public static int subtract(int a, int b){
-        return a + b;
+        return a - b;
     }
     public static void main(String[] args) {
         Cal cal = new Cal();
-        System.out.println("请输入一个数");
+        System.out.println("one num");
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        System.out.println("请输入另一个数");
+        System.out.println("another num");
         int b = in.nextInt();
         int c = Cal.add(a,b);
         int d = Cal.subtract(a,b);
-        System.out.println("加法的结果为" + c);
-        System.out.println("减法的结果为" + c);
+        System.out.println("addnum" + c);
+        System.out.println("subtractnum" + d);
     }
 }
